651572f519821623fcff4f3c2623efe47ee6b746
Mizaer Paris <mizael.leite@dcomp.ufs.br>
2024-09-18 20:13:55 -0300

    acrescentando o object freeze

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const criarAluno = (nome, idade, matricula, curso) => ({ nome, idade, matricula, curso }); // tarefa 1
+const criarAluno = (nome, idade, matricula, curso) => Object.freeze(({ nome, idade, matricula, curso })); // tarefa 1
 
 const turma = Object.freeze([]); // tarefa 2
 
 const adicionarAluno = (turma, nome, idade, matricula, curso) =>  // tarefa 3
   [...turma, criarAluno(nome, idade, matricula, curso)];
 
-const turma1 = adicionarAluno(turma, 'João', 20, 20240003, 'CC')
-const turma2 = adicionarAluno(turma1, 'Fagundes', 21, 20240001, 'CC')
-const turma3 = adicionarAluno(turma2, 'Ricardo', 20, 20240004, 'EC')
-const turma4 = adicionarAluno(turma3, 'Felipe', 19, 20240002, 'EC')
-const turma5 = adicionarAluno(turma4, 'Pedro', 17, 20240005, 'CC')
-const turma6 = adicionarAluno(turma5, 'Maria', 17, 20240006, 'SI')
+const turma1 = Object.freeze(adicionarAluno(turma, 'João', 20, 20240003, 'CC'))
+const turma2 = Object.freeze(adicionarAluno(turma1, 'Fagundes', 21, 20240001, 'CC'))
+const turma3 = Object.freeze(adicionarAluno(turma2, 'Ricardo', 20, 20240004, 'EC'))
+const turma4 = Object.freeze(adicionarAluno(turma3, 'Felipe', 19, 20240002, 'EC'))
+const turma5 = Object.freeze(adicionarAluno(turma4, 'Pedro', 17, 20240005, 'SI'))
+const turma6 = Object.freeze(adicionarAluno(turma5, 'Maria', 17, 20240006, 'SI'))
 
 const listarAlunos = (lista) => lista.filter((aluno) => aluno) // tarefa 4
 
@@ -33,4 +33,4 @@ const editarAluno = (matricula,turma) => (novosDados) => {
     )
 }
 
-const turma7 = editarAluno(20240001, turma6)({idade: 72}); // testando função editar aluno
+const result = editarAluno(20240001,turma6)({nome:'Guilherme', idade: 17, matricula: 20240030, curso: 'SI'}) // teste função editarAluno
